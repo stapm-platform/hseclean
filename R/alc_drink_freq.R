f99ed41c940961c3b9bfd2e717c18066c39f0b3b
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2020-05-28 11:38:44 +0100

    update alc drink freq description

--- R/alc_drink_freq.R
+++ R/alc_drink_freq.R
@@ -5,7 +5,15 @@
 #'
 #' @param x Drinking frequency
 #'
-#' @return Returns a numeric translation of the categorical drinking frequency variable.
+#' @return Returns a numeric translation (days/week) of the categorical drinking frequency variable.
+#' \item{Almost every day}{7}
+#' \item{Five or six days a week}{5.5}
+#' \item{Three or four days a week}{3.5}
+#' \item{Once or twice a week}{1.5}
+#' \item{Once or twice a month}{0.375}
+#' \item{Once every couple of months}{0.188}
+#' \item{Once or twice a year}{0.029}
+#' 
 #' @export
 #'
 #' @examples
