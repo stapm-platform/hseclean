c4249ef0a41849dedb3030042f8db894dff2aa96
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2020-08-04 15:16:20 +0100

    use external population numbers to re-calculate survey weights

--- R/clean_surveyweights.R
+++ R/clean_surveyweights.R
@@ -1,6 +1,9 @@
 
 
 #' Process the survey weights
+#' 
+#' Makes new survey weights based on the difference between the HSE sample size and the actual population size 
+#' by year, age, sex and IMD quintiles.   
 #'
 #' @param data Data table - the Health Survey for England dataset
 #' @param pop_data Data table - population counts
@@ -12,32 +15,32 @@
 #'
 #' \dontrun{
 #'
-#' data <- clean_surveyweights(data)
+#' data <- clean_surveyweights(data, pop_data = stapmr::pop_counts)
 #'
 #' }
 #'
 clean_surveyweights <- function(
   data,
-  pop_data = hseclean::pop_counts
+  pop_data
 ) {
 
   # Fill any missing survey weights
-  data[ , mean_wt_int := mean(wt_int, na.rm = T), by = "year"]
-  data[is.na(mean_wt_int), mean_wt_int := 1]
-  data[is.na(wt_int), wt_int := mean_wt_int]
-  data[ , mean_wt_int := NULL]
+  #data[ , mean_wt_int := mean(wt_int, na.rm = T), by = "year"]
+  #data[is.na(mean_wt_int), mean_wt_int := 1]
+  #data[is.na(wt_int), wt_int := mean_wt_int]
+  #data[ , mean_wt_int := NULL]
 
   # Make survey weights sum to 1 within each year
-  data[ , wt_int := wt_int / sum(wt_int, na.rm = T), by = "year"]
-
-  # Adjust weights by population size
-  #data <- merge(data, pop_data, by = c("year", "age", "sex", "imd_quintile"), all.x = T)
-  #data[ , sample_n := .N, by = c("year", "age", "sex", "imd_quintile")]
-  #data[ , wt_pop := sample_n / N]
-  #data[ , wt_int := wt_pop * wt_int]
   #data[ , wt_int := wt_int / sum(wt_int, na.rm = T), by = "year"]
 
-  #data[ , `:=`(sample_n = NULL, N = NULL, wt_pop = NULL)]
+  # Adjust weights by population size
+  data <- merge(data, pop_data, by = c("year", "age", "sex", "imd_quintile"), all.x = T)
+  data[ , sample_n := .N, by = c("year", "age", "sex", "imd_quintile")]
+  data[ , wt_int := N / sample_n]
+  data[ , wt_int := wt_int / sum(wt_int, na.rm = T), by = "year"]
+
+  data[ , `:=`(sample_n = NULL, N = NULL)]
+  
 
 return(data[])
 }
