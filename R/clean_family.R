46edd9d5a719b8da765793b1fe8e16c77c4d29d1
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2022-03-31 11:39:42 +0100

    impute scottish kids

--- R/clean_family.R
+++ R/clean_family.R
@@ -24,10 +24,10 @@
 #' }
 #'
 #' @param data Data table - the Health Survey for England dataset.
-#' 
+#'
 #' @importFrom data.table :=
 #' @importFrom stats predict
-#' 
+#'
 #' @return Returns an updated version of data with the new family variables:
 #' \itemize{
 #' \item 'kids' - number of children age <= 15 years in the household (0, 1, 2, 3+)
@@ -40,12 +40,12 @@
 #' \dontrun{
 #'
 #' library(hseclean)
-#' 
+#'
 #' #data <- read_2015(root = "/Volumes/Shared/")
-#' 
+#'
 #' storage_location <- "/Users/duncangillespie/Documents/HSE/"
 #' data <- readRDS(paste0(storage_location, "HSE_2015.rds"))
-#' 
+#'
 #' data <- clean_age(data)
 #' data <- clean_demographic(data)
 #' data <- clean_education(data)
@@ -59,77 +59,83 @@ clean_family <- function(
 ) {
 
   country <- unique(data[ , country][1])
-  
+
 
   #####################################################
   # Relationship status (England)
+
   if(country == "England"){
-  data[marstat == 1, relationship_status := "single"]
 
-  # Married, civil partnership or cohabiting
-  data[marstat == 2, relationship_status := "married"]
-  data[marstat == 6, relationship_status := "cohabit"]
+    data[marstat == 1, relationship_status := "single"]
 
-  # Separated, divorced or widowed
-  data[marstat %in% c(3, 4, 5), relationship_status := "sep_div_wid"]
+    # Married, civil partnership or cohabiting
+    data[marstat == 2, relationship_status := "married"]
+    data[marstat == 6, relationship_status := "cohabit"]
 
-  # If under 18 and missing, assume single
-  data[is.na(relationship_status) & age < 18, relationship_status := "single"]
-  data[ , marstat := NULL]
+    # Separated, divorced or widowed
+    data[marstat %in% c(3, 4, 5), relationship_status := "sep_div_wid"]
+
+    # If under 18 and missing, assume single
+    data[is.na(relationship_status) & age < 18, relationship_status := "single"]
+    data[ , marstat := NULL]
+
   }
 
   # Relationship status (Scotland)
+
   if(country == "Scotland"){
-  data[maritalg == 3, relationship_status := "single"]
-  
-  # Married, civil partnership or cohabiting
-  data[maritalg %in% 1:2, relationship_status := "married"]
-  
-  # Separated, divorced or widowed
-  data[maritalg %in% c(4, 5, 6), relationship_status := "sep_div_wid"]
-  
-  # If under 18 and missing, assume single
-  data[is.na(relationship_status) & age < 18, relationship_status := "single"]
-  data[ , maritalg := NULL]
+
+    data[maritalg == 3, relationship_status := "single"]
+
+    # Married, civil partnership or cohabiting
+    data[maritalg %in% 1:2, relationship_status := "married"]
+
+    # Separated, divorced or widowed
+    data[maritalg %in% c(4, 5, 6), relationship_status := "sep_div_wid"]
+
+    # If under 18 and missing, assume single
+    data[is.na(relationship_status) & age < 18, relationship_status := "single"]
+    data[ , maritalg := NULL]
+
   }
-  
+
   #####################################################
   # Number of children in household
-  
+
   # This variable now not possible to get from data 2015+ or from SHeS
-  
+
   # for years 2015+, initially set the number of children and infants to NA
   data[year >= 2015 | country == "Scotland", `:=`(infants = NA, children = NA)]
-  
+
   # Sum the number of infants and children
   data[ , kids := children + infants]
-  
+
   # Change the variable from numeric to 4 categories
   data[kids > 3, kids := 3]
   data[ , kids := as.character(kids)]
   data[kids == "3", kids := "3+"]
-  
+
   # For years < 2015,
   # if number of children is missing and age is less than 16 years,
   # assume no children
   data[country == "England" & year < 2015 & is.na(kids) & age < 16, kids := "0"]
-  
+
   # Remove variables no longer required
   data[ , `:=`(infants = NULL, children = NULL)]
-  
+
   # For years >= 2015, impute the number of kids
   required_vars <- c("age_cat", "sex", "relationship_status", "ethnicity_4cat", "imd_quintile", "eduend4cat", "degree", "nssec3_lab", "employ2cat", "activity_lstweek")
-  
+
   testthat::expect_equal(
     length(required_vars),
     length(intersect(required_vars, colnames(data))),
     info = "one of these variables is missing from data:
   age_cat, sex, relationship_status, ethnicity_4cat, imd_quintile, eduend4cat, degree, nssec3_lab, employ2cat, activity_lstweek"
   )
-    
+
   # impute kids
-  data[year >= 2015, kids := predict(hseclean::impute_kids_model, newdata = data)]
-  
-  
-return(data[])
+  data[year >= 2015 | country == "Scotland", kids := predict(hseclean::impute_kids_model, newdata = data)]
+
+
+  return(data[])
 }
